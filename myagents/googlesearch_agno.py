0228e5d1544cca8bc00f0221be5b162e2be45fc7
ybonda <ybonda@gmail.com>
2025-05-26 10:13:38 +0300

    prompt update

--- myagents/googlesearch_agno.py
+++ myagents/googlesearch_agno.py
@@ -26,14 +26,15 @@ agent = Agent(
         " - name: crawl the name of the tool from the tool webpage content.",
         " - short_description: short description of the tool (12 words max).",
         " - description: detailed description of the tool (50-200 words)",
-        " - how_it_works: Explain how the tool works.",
-        " - how_to _use: Explain how to use the tool.",
-        " - use_cases: Explain what are use cases.",
+        " - how_it_works: Based on the search results explain how the tool works.",
+        " - how_to _use: Based on the search results explain how to use the tool.",
+        " - use_cases: Based on the search results explain what are use cases.",
         " - pros: Provide 1-4 strong pros for why this tool is better than the alternatives based on actual search results.",
         " - cons: Provide 1-3 strong cons for why this tool is worse than the alternatives based on actual search results.",
         " - alternatives: Provide 1-3 alternative tools.",
         " - tags: select 1-2 best matching tags ONLY from the list: Project Management, Business Forecasting, Business Automation, Social Media, Ethical AI Tools, Virtual Performers, Chatbots, Content Strategy, 3D Design, Fashion AI, Academic Writing, Writing Tools, Graphic Design, DevOps Assistants, Life Assistants, Fitness Apps, Automation Tools, Real Estate Analysis, Prompt Generators, Mental Health, Meeting Assistants, Travel Planning, Non-profit Solutions, Video Editing, No-Code Platform, Animation, Security, Coding Assistants, E-learning, Medical AI, Speech-to-Text, Science, App Builders, Blockchain AI, Environmental Monitoring, Chat Summarizers, Text-to-3D, Data Analysis, Fun AI, E-commerce Tools, Investment Tools, Robotics, Music Generation, Avatar Generation, Language Learning, Compliance, Gaming AI, Coocking AI, Quantum Computing AI, API, AI for Social Good, Knowledge Management, Marketing Tools, Resume Builders, Financial Analysis, Manufacturing AI, News, Art Generators, Research, Presentation, Storytelling, Video Generation, Translation, SEO Tools, Personal Development, Voice Assistants, Image Generation, Food AI, Voice Generation, Event Planning, Consulting, Adult Content, Storyboarding, Legal AI Tools, VR & AR, Task Management, Human Resources, Sports AI, Telemedicine, Recruitment, Customer Service, Vibe Coding, Parenting AI, Business Intelligence, Dating AI, Medical Diagnosis, CRM, Image to Video, Text to Video, Video to Video."
         "- category: select the best matching single category ONLY from the list: Healthcare & Wellness, Gaming & Entertainment, Developer Tools, Image & Art, Education & Research, Productivity, 3D & Spatial, Audio & Voice, Lifestyle & Personal, Professional Services, Communication, News & Media, Business & Finance, Video & Animation, Content Creation, Social Impact, NSFW, Industry-Specific AI.",
+        "Avoid in the response uncertainties, hedging, and phrases like 'I think', 'I believe', 'it seems', 'it looks like', 'it appears'.",
 
         # " Crawl pricing page and provide the pricing information.",
         # " - pricing_plans: an array of pricing plans, if pricing is available.",
@@ -50,3 +51,4 @@ agent = Agent(
 # agent.print_response("AI tool website: https://www.tavily.com/")
 # agent.print_response("AI tool website: https://cline.bot/")
 agent.print_response("AI tool website: https://www.trynia.ai/")
+# agent.print_response("AI tool website: https://www.get-teleprompt.com/")
